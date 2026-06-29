8314c300936101fb9ae5fa7c34b1e50769bff67b
swamysamineedi <samineediswamy@gmail.com>
2026-06-29 16:02:00 +0530

    Added Work Wagon project and updated portfolio

--- script.js
+++ script.js
@@ -672,3 +672,142 @@ if(contactForm){
     });
 
 }
+
+
+
+const canvas=document.getElementById("network-bg");
+const ctx=canvas.getContext("2d");
+
+function resize(){
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
+}
+resize();
+window.addEventListener("resize",resize);
+
+const mouse={
+x:null,
+y:null,
+radius:140
+};
+
+window.addEventListener("mousemove",e=>{
+mouse.x=e.x;
+mouse.y=e.y;
+});
+
+const particles=[];
+const count=90;
+
+for(let i=0;i<count;i++){
+
+particles.push({
+
+x:Math.random()*canvas.width,
+y:Math.random()*canvas.height,
+
+vx:(Math.random()-0.5)*0.5,
+vy:(Math.random()-0.5)*0.5,
+
+size:2+Math.random()*2
+
+});
+
+}
+
+function animate(){
+
+ctx.clearRect(0,0,canvas.width,canvas.height);
+
+for(let i=0;i<particles.length;i++){
+
+let p=particles[i];
+
+p.x+=p.vx;
+p.y+=p.vy;
+
+if(p.x<0||p.x>canvas.width)p.vx*=-1;
+if(p.y<0||p.y>canvas.height)p.vy*=-1;
+
+let dx=mouse.x-p.x;
+let dy=mouse.y-p.y;
+
+let dist=Math.sqrt(dx*dx+dy*dy);
+
+if(dist<mouse.radius){
+
+p.x-=dx*0.002;
+p.y-=dy*0.002;
+
+}
+
+ctx.beginPath();
+
+ctx.arc(
+p.x,
+p.y,
+p.size,
+0,
+Math.PI*2
+);
+
+ctx.fillStyle="#3B82F6";
+
+ctx.shadowBlur=15;
+ctx.shadowColor="#3B82F6";
+
+ctx.fill();
+
+}
+
+for(let i=0;i<particles.length;i++){
+
+for(let j=i+1;j<particles.length;j++){
+
+let dx=particles[i].x-particles[j].x;
+let dy=particles[i].y-particles[j].y;
+
+let distance=Math.sqrt(dx*dx+dy*dy);
+
+if(distance<130){
+
+ctx.beginPath();
+
+ctx.moveTo(
+particles[i].x,
+particles[i].y
+);
+
+ctx.lineTo(
+particles[j].x,
+particles[j].y
+);
+
+ctx.strokeStyle=`rgba(59,130,246,${1-distance/130})`;
+
+ctx.lineWidth=1;
+
+ctx.stroke();
+
+}
+
+}
+
+}
+
+requestAnimationFrame(animate);
+
+}
+
+animate();
+
+
+/* fbhbfhbhfbh*/
+
+
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove",(e)=>{
+    glow.style.left = e.clientX + "px";
+    glow.style.top = e.clientY + "px";
+});
